f34a2af4cc57bdd634418e9cee30ff72644003cb
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-04-26 18:44:37 +0500

    Comments added

--- Bro_Code/Tutorial_One/index.js
+++ Bro_Code/Tutorial_One/index.js
@@ -1,7 +1,9 @@
+// Displays various texts on console window.
 console.log("Hello World");
 console.log("I Like Pizza!");
 console.log("It's Really Good!");
 
+// Displays alert box with message.
 window.alert("Hello World")
 
 // This is a single line comment.
