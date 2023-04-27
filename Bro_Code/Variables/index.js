7562fc3bd1946a3b52c639ae980c777795af80b9
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-04-27 11:20:16 +0500

    Comments added

---
+++ Bro_Code/Variables/index.js
@@ -0,0 +1,6 @@
+// A variable is a container for storing data
+// A variable behaves as if it was the value it contains
+
+// Two steps:
+// 1. Declaration (var, let, const)
+// 2. Assignment (= assignment operator)
