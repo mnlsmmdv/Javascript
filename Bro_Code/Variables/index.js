2de69a7dfe8e62ca76114605247fbf67d1de4473
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-04-27 11:31:59 +0500

    Displays incremented value

--- Bro_Code/Variables/index.js
+++ Bro_Code/Variables/index.js
@@ -12,3 +12,9 @@ let second_age = 23;
 
 console.log("Your age is: " + age);
 console.log("Your second age is: " + second_age);
+
+// Incrementing to a variable + displaying value.
+let third_age = 50;
+fourth_age = third_age + 1;
+
+console.log("Your fourth age is: " + fourth_age);
