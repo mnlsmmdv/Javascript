41e2638fe1fb0e714857e580a1f6f6c6e79f0cc9
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-04-27 11:27:37 +0500

    Displays variable declaration using two methods

--- Bro_Code/Variables/index.js
+++ Bro_Code/Variables/index.js
@@ -4,3 +4,11 @@
 // Two steps:
 // 1. Declaration (var, let, const)
 // 2. Assignment (= assignment operator)
+
+// Declaring variables + displaying value.
+let age;
+age = 21;
+let second_age = 23;
+
+console.log("Your age is: " + age);
+console.log("Your second age is: " + second_age);
