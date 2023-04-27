b2b877f7a1a32a369642d0562a86dc6862f9b1ed
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-04-27 13:41:27 +0500

    Comment added

---
+++ Bro_Code/Arithmetic_Expressions/index.js
@@ -0,0 +1,7 @@
+/*
+    Arithmetic expressions is a combination of...
+    operands (values, variables, etc.)
+    operators (+ - * / %)
+    that can be evaluated to a value.
+    ex. y = x + 5;
+*/
