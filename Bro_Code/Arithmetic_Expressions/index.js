a2df4f8709f232cb187f673fa8730c94b294afd9
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-04-28 10:46:03 +0500

    Variable declared + Addition example

--- Bro_Code/Arithmetic_Expressions/index.js
+++ Bro_Code/Arithmetic_Expressions/index.js
@@ -5,3 +5,10 @@
     that can be evaluated to a value.
     ex. y = x + 5;
 */
+
+// Variable declaration.
+students = 23;
+
+// Addition.
+addition = students + 1;
+console.log("Addition: " + addition)
