a0bc25ab42cd80557f48c8b5dd7de618b506085d
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-04-29 16:48:39 +0500

    Variable name fixed

--- Bro_Code/Type_Conversion/index.js
+++ Bro_Code/Type_Conversion/index.js
@@ -3,10 +3,10 @@
 // Variable declarations.
 let pi = 3.141592653589793238462643383279502884197;
 let radius;
-let circumference;
+let circleCircumference;
 
 // This will ask for radius and check the circumference of a circle.
 circleRadius = window.prompt("Enter The Radius: ");
 circleRadius = Number(circleRadius);
 circleCircumference = 2 * pi * circleRadius;
-console.log("The Circumference of a Circle is: " + circleCumference);
+console.log("The Circumference of a Circle is: " + circleCircumference);
