cd62d66f0847ee3f991600d635fb5c19acec8bb2
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-04-29 11:40:52 +0500

    Will change inner HTML document

--- Bro_Code/User_Input/index.js
+++ Bro_Code/User_Input/index.js
@@ -1,3 +1,11 @@
 // Accepting user input using a Javascript window prompt.
 let username = window.prompt("Enter Your Username: ");
 console.log(username);
+
+// Accepting user input using an HTML text box and button.
+let usernameTwo;
+document.getElementById("myButton").onclick = function(){
+    usernameTwo = document.getElementById("myText").value;
+    console.log("Your Username: " + usernameTwo);
+    document.getElementById("myLabel").innerHTML = "Hello " + usernameTwo;
+}
