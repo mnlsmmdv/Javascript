8aa35fe148a37d947e9c08d42aaa962709f5ca79
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-04-29 16:51:07 +0500

    Value of PI is now a constant

--- Bro_Code/Type_Conversion/index.js
+++ Bro_Code/Type_Conversion/index.js
@@ -1,7 +1,7 @@
 // const = A variable that can not be changed.
 
 // Variable declarations.
-let pi = 3.141592653589793238462643383279502884197;
+const pi = 3.141592653589793238462643383279502884197;
 let radius;
 let circleCircumference;
 
