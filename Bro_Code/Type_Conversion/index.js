b6b0ad94547fa9ae86ddbea1074d8f0bd6fc6c8e
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-04-30 13:43:33 +0500

    Displays age but with an error cause no type conversion

--- Bro_Code/Type_Conversion/index.js
+++ Bro_Code/Type_Conversion/index.js
@@ -2,3 +2,8 @@
     Type conversion = Changing the datatype of a value to another
     (strings, numbers, booleans)
 */
+
+// User input example with type conversion.
+let age = window.prompt("How Old Are You?: ");
+age += 1;
+console.log("Happy Birthday! You are ", age, " years old.");
