f682e3ef22628fe54c2995651e6cbcf7a0c7f9e6
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-04-30 19:40:46 +0500

    Displays rounded off value

--- Bro_Code/Math/index.js
+++ Bro_Code/Math/index.js
@@ -1,1 +1,6 @@
 // An intrinsic object that provides basic mathematics functionality and constants.
+
+// Rounding variable example.
+let x = 3.14;
+x = Math.round(x);
+console.log("Value rounded: " + x);
