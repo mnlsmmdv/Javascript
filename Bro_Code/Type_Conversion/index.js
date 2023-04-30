19e1dfd83b153dc82f1258863c44710e091b80b6
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-04-30 13:56:49 +0500

    Displays current type of converted values

--- Bro_Code/Type_Conversion/index.js
+++ Bro_Code/Type_Conversion/index.js
@@ -22,3 +22,7 @@ x = Number("3.14");
 y = String(3.14);
 // Converting a value to a Boolean.
 z = Boolean("");
+// Displaying current type of converted values.
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
