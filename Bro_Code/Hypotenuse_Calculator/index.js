e4a802de0f506a8fd11a105ce5ee69c3842725e2
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-01 11:05:20 +0500

    Calculates the Hypotenuse using two methods

--- Bro_Code/Hypotenuse_Calculator/index.js
+++ Bro_Code/Hypotenuse_Calculator/index.js
@@ -8,3 +8,11 @@ a = Number(a); // Type conversion to a Number.
 
 b = window.prompt("Enter side B: "); // Asking for user input side B.
 b = Number(b); // Type conversion to a number.
+
+/* Longer version.
+c = Math.pow(a, 2) + Math.pow(b, 2);
+c = Math.sqrt(c);
+*/
+
+c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)); // Shorter version to calculate Hypotenuse.
+console.log("Side C is: ", c);
