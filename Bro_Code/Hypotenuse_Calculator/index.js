3839c3036ad8b49dcabe87a6ed929317f0e0087a
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-01 11:06:46 +0500

    Code commented out

--- Bro_Code/Hypotenuse_Calculator/index.js
+++ Bro_Code/Hypotenuse_Calculator/index.js
@@ -3,11 +3,13 @@ let a;
 let b;
 let c;
 
+/*
 a = window.prompt("Enter side A: "); // Asking for user input side A.
 a = Number(a); // Type conversion to a Number.
 
 b = window.prompt("Enter side B: "); // Asking for user input side B.
 b = Number(b); // Type conversion to a number.
+*/
 
 /* Longer version.
 c = Math.pow(a, 2) + Math.pow(b, 2);
