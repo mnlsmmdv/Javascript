cdb3691ec0e7d2ee32633f8ce318ed08db632172
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-01 11:00:13 +0500

    Asks for inputs for side A and and B

--- Bro_Code/Hypotenuse_Calculator/index.js
+++ Bro_Code/Hypotenuse_Calculator/index.js
@@ -2,3 +2,9 @@
 let a;
 let b;
 let c;
+
+a = window.prompt("Enter side A: "); // Asking for user input side A.
+a = Number(a); // Type conversion to a Number.
+
+b = window.prompt("Enter side B: "); // Asking for user input side B.
+b = Number(b); // Type conversion to a number.
