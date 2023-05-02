4ed22992935748f679236d6dad3be5d8f5b34a6d
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-02 13:07:15 +0500

    Generates random numbers for labels

--- Bro_Code/Random_Number_Generator/index.js
+++ Bro_Code/Random_Number_Generator/index.js
@@ -2,6 +2,12 @@
     Random Number Generator
 */
 
+// Variable declarations.
+// Simulates a 20 sided dice.
+let numberOne = Math.floor(Math.random() * 20) + 1;
+let numberTwo = Math.floor(Math.random() * 20) + 1;
+let numberThree = Math.floor(Math.random() * 20) + 1;
+
 // This function resets the labels.
 document.getElementById("resetButton").onclick = function(){
     document.getElementById("randomNumber1").innerHTML = "0";
