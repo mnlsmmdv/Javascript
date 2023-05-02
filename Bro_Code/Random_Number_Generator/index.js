c93a92abf2ad0b78b6680a10c3ae3da6b967bffb
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-02 13:12:43 +0500

    Variables now declared inside function

--- Bro_Code/Random_Number_Generator/index.js
+++ Bro_Code/Random_Number_Generator/index.js
@@ -2,14 +2,14 @@
     Random Number Generator
 */
 
-// Variable declarations.
-// Simulates a 06 sided dice.
-let numberOne = Math.floor(Math.random() * 6) + 1;
-let numberTwo = Math.floor(Math.random() * 6) + 1;
-let numberThree = Math.floor(Math.random() * 6) + 1;
-
 // This function displays the generated random numbers.
 document.getElementById("rollButton").onclick = function(){
+    // Variable declarations.
+    // Simulates a 06 sided dice.
+    let numberOne = Math.floor(Math.random() * 6) + 1;
+    let numberTwo = Math.floor(Math.random() * 6) + 1;
+    let numberThree = Math.floor(Math.random() * 6) + 1;
+
     document.getElementById("randomNumber1").innerHTML = numberOne;
     document.getElementById("randomNumber2").innerHTML = numberTwo;
     document.getElementById("randomNumber3").innerHTML = numberThree;
