3e74bcd7412fc86fa5d3f1f56918e243e7a37aee
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-02 13:11:36 +0500

    Simulates six sided dice

--- Bro_Code/Random_Number_Generator/index.js
+++ Bro_Code/Random_Number_Generator/index.js
@@ -3,10 +3,10 @@
 */
 
 // Variable declarations.
-// Simulates a 20 sided dice.
-let numberOne = Math.floor(Math.random() * 20) + 1;
-let numberTwo = Math.floor(Math.random() * 20) + 1;
-let numberThree = Math.floor(Math.random() * 20) + 1;
+// Simulates a 06 sided dice.
+let numberOne = Math.floor(Math.random() * 6) + 1;
+let numberTwo = Math.floor(Math.random() * 6) + 1;
+let numberThree = Math.floor(Math.random() * 6) + 1;
 
 // This function displays the generated random numbers.
 document.getElementById("rollButton").onclick = function(){
