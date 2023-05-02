a6f6c2eda5804ee76cc838808cd89cb0398a4ac5
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-02 13:11:06 +0500

    Displays random numbers on button click

--- Bro_Code/Random_Number_Generator/index.js
+++ Bro_Code/Random_Number_Generator/index.js
@@ -8,6 +8,13 @@ let numberOne = Math.floor(Math.random() * 20) + 1;
 let numberTwo = Math.floor(Math.random() * 20) + 1;
 let numberThree = Math.floor(Math.random() * 20) + 1;
 
+// This function displays the generated random numbers.
+document.getElementById("rollButton").onclick = function(){
+    document.getElementById("randomNumber1").innerHTML = numberOne;
+    document.getElementById("randomNumber2").innerHTML = numberTwo;
+    document.getElementById("randomNumber3").innerHTML = numberThree;
+}
+
 // This function resets the labels.
 document.getElementById("resetButton").onclick = function(){
     document.getElementById("randomNumber1").innerHTML = "0";
