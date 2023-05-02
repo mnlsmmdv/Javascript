c089b8acd4c7c1d47e381c9e1eac2c52e86d888e
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-02 12:56:26 +0500

    Will now reset three labels on button click

---
+++ Bro_Code/Random_Number_Generator/index.js
@@ -0,0 +1,10 @@
+/*
+    Random Number Generator
+*/
+
+// This function resets the labels.
+document.getElementById("resetButton").onclick = function(){
+    document.getElementById("randomNumber1").innerHTML = "0";
+    document.getElementById("randomNumber2").innerHTML = "0";
+    document.getElementById("randomNumber3").innerHTML = "0";
+}
