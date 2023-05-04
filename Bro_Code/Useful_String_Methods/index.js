83b570af1a007587ae1a51a8db9f32e4582255e4
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-04 19:57:45 +0500

    Displays the length of the string

---
+++ Bro_Code/Useful_String_Methods/index.js
@@ -0,0 +1,8 @@
+/*
+    Useful String Methods
+*/
+
+// Displays the length of string.
+let userName = "mnlsmmdv";
+let nameLength = userName.length;
+console.log("Length of Username: " + nameLength);
