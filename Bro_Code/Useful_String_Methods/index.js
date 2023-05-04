cba1fc2e7607fdbf01dba114c06e9b6dd2721b60
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-04 20:04:36 +0500

    Displays index of given character

--- Bro_Code/Useful_String_Methods/index.js
+++ Bro_Code/Useful_String_Methods/index.js
@@ -6,3 +6,8 @@
 let userName = "mnlsmmdv";
 let nameLength = userName.length;
 console.log("Length of Username: " + nameLength);
+
+// Displays the index of a character in a given string.
+let userNameTwo = "mnlsmmdv";
+let nameIndex = userNameTwo.charAt(6);
+console.log("Index of Character: " + nameIndex);
