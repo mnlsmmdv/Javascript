48eb8141114df264361db5939203710ef773174f
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-06 22:05:58 +0500

    Comment added

--- Bro_Code/String_Slicing/index.js
+++ Bro_Code/String_Slicing/index.js
@@ -2,6 +2,7 @@
 //         and returns it as a new string,
 //         without modifying the original string
 
+// Variable declaration.
 let fullName = "Ahmed Affaan";
 let firstName;
 let lastName;
