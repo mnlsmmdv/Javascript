3c19b252ef0cc753cdf614b4f39fb9387040803b
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-06 22:05:34 +0500

    Variables declared

--- Bro_Code/String_Slicing/index.js
+++ Bro_Code/String_Slicing/index.js
@@ -1,3 +1,7 @@
 // slice() extracts a section of a string
 //         and returns it as a new string,
 //         without modifying the original string
+
+let fullName = "Ahmed Affaan";
+let firstName;
+let lastName;
