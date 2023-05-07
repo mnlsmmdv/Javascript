eb57a24b0c59cbe3946baacc9c1badbc728b9ea1
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-07 14:47:41 +0500

    Code commented out

--- Bro_Code/Method_Chaining/index.js
+++ Bro_Code/Method_Chaining/index.js
@@ -2,7 +2,9 @@
 //                   in one continous line of code
 
 // Capitalising one single letter - Method One.
+/*
 let userName = "mnlsmmdv";
 let letter = userName.charAt(0);
 letter = letter.toUpperCase();
 console.log("Username uppercase letter: " + letter);
+*/
