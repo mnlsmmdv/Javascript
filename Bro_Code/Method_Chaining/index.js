5237753c5cb14fc93385bfc14b9aff9fa208de38
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-07 14:47:20 +0500

    Capitalises the first letter of the username

--- Bro_Code/Method_Chaining/index.js
+++ Bro_Code/Method_Chaining/index.js
@@ -1,2 +1,8 @@
 // Method Chaining = Calling one method after another
 //                   in one continous line of code
+
+// Capitalising one single letter - Method One.
+let userName = "mnlsmmdv";
+let letter = userName.charAt(0);
+letter = letter.toUpperCase();
+console.log("Username uppercase letter: " + letter);
