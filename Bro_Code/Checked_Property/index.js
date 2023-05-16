70eca9001519f60cc68d4c165e81bd499f8f322a
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-16 22:12:15 +0500

    Syntax fixes

--- Bro_Code/Checked_Property/index.js
+++ Bro_Code/Checked_Property/index.js
@@ -1,10 +1,10 @@
 /*This function validates the checkbox with button clicks*/
 document.getElementById("myButton").onclick = function(){
     // Constants declared to retrieve element ID.
-    const myCheckBox = document.getElementById("myCheckBox")
-    const visaButton = document.getElementById("visaButton")
-    const mastercardButton = document.getElementById("mastercardButton")
-    const americanexpressButton = document.getElementById("americanexpressButton")
+    const myCheckBox = document.getElementById("myCheckBox");
+    const visaButton = document.getElementById("visaButton");
+    const mastercardButton = document.getElementById("mastercardButton");
+    const americanexpressButton = document.getElementById("americanexpressButton");
     if(myCheckBox.checked){
         // Displays the user is subscribed.
         console.log("You Are Subscribed!");
