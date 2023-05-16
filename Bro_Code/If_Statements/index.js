b5432d65368e9c62d9b423c669dc7433ad382b7c
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-16 14:41:16 +0500

    Displays messages if user is over 18 years old

--- Bro_Code/If_Statements/index.js
+++ Bro_Code/If_Statements/index.js
@@ -1,3 +1,17 @@
 // if statement = A basic form of decision making
 //                if a condition is true, then do something
 //                if not, then don't do it!
+
+// Variable declarations.
+let age = 21;
+
+// Displays message is user is over 18 years old.
+if(age >= 18){
+    console.log("You are an adult!");
+}
+else if(age < 0){
+    console.log("You haven't been born yet!")
+}
+else{
+    console.log("You are a child!");
+}
