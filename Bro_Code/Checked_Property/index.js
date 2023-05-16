9c3a54cf798e0599c2799accdd870b1fa1c693ac
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-16 19:04:02 +0500

    Comments added

--- Bro_Code/Checked_Property/index.js
+++ Bro_Code/Checked_Property/index.js
@@ -2,9 +2,11 @@
 document.getElementById("myButton").onclick = function(){
     const myCheckBox = document.getElementById("myCheckBox")
     if(myCheckBox.checked){
+        // Displays the user is subscribed.
         console.log("You Are Subscribed!");
     }
     else{
+        // Displays the user is not subscribed.
         console.log("You Are Not Subscribed!");
     }
 }
