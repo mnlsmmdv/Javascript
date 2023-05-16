dc1b094c3d5bf3549f9ac95f90e8ce3bea5fb38b
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-17 01:40:06 +0500

    Variable declared

--- Bro_Code/Switches/index.js
+++ Bro_Code/Switches/index.js
@@ -1,3 +1,6 @@
 // switch = Statement that examines a value
 //          for a match against many case clauses.
 //          More efficient than many "else if" statements
+
+// Variable declaration for grade.
+let grade = "A";
