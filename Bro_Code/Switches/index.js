541d87051021ef0fc771b3eff1bcc78399c8a5df
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-17 01:48:35 +0500

    Case clauses for grade

--- Bro_Code/Switches/index.js
+++ Bro_Code/Switches/index.js
@@ -4,3 +4,36 @@
 
 // Variable declaration for grade.
 let grade = "A";
+
+// Case clauses for the grade variable.
+switch(grade){
+    // Grade is A.
+    case "A":
+        // Message given to user.
+        console.log("You did great!");
+        break;
+    // Grade is B.
+    case "B":
+        // Message given to user.
+        console.log("You did good!");
+        break;
+    // Grade is C.
+    case "C":
+        // Message given to user.
+        console.log("You did okay!");
+        break;
+    // Grade is D.
+    case "D":
+        // Message given to user.
+        console.log("You passed...barely!");
+        break;
+    // Grade is F.
+    case "F":
+        // Message given to user.
+        console.log("You failed!");
+        break;
+    // No matching grade.
+    default:
+        // Message given to user.
+        console.log(grade, "is not a letter grade!");
+}
