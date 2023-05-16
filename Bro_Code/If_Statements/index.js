63be5fffd4e522a5dd87216863c2204b2b48dc33
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-16 14:51:27 +0500

    Displays message if user is above 65 years old

--- Bro_Code/If_Statements/index.js
+++ Bro_Code/If_Statements/index.js
@@ -40,3 +40,17 @@ else if(ageThree < 0){
 else{
     console.log("You are a child!")
 }
+
+// Displays message if user is above 65 years old.
+if(ageFour >= 18){
+    console.log("You are an adult!");
+}
+else if(ageFour < 0){
+    console.log("You haven't been born yet!");
+}
+else if(ageFour >= 65){
+    console.log("You are a senior citizen!");
+}
+else{
+    console.log("You are a child!");
+}
