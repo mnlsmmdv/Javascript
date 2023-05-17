872db4d671c6d3298d10c13549af1be2bffb06fd
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-17 14:43:53 +0500

    Variable declared for temperature

--- Bro_Code/AND_OR_logical_operators/index.js
+++ Bro_Code/AND_OR_logical_operators/index.js
@@ -1,3 +1,6 @@
 // Gives us the ability to check more than 1 condition concurrently
 // && AND (BOTH conditions must be true)
 // || OR (Either condition can be true)
+
+// Variable declaration for temperate.
+let temperature = 15;
