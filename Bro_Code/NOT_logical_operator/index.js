d0e73c2e9bb73fdc35d85f6a5222270c3fdb6761
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-18 22:15:10 +0500

    Variables declarations for weather and temperature

--- Bro_Code/NOT_logical_operator/index.js
+++ Bro_Code/NOT_logical_operator/index.js
@@ -1,3 +1,7 @@
 // ! NOT logical operator
 // typically used to reverse a condition's boolean value
 // true -> false false -> true
+
+// Variable declaration for temperature and sunny weather.
+let temperature = 15;
+let sunny = true;
