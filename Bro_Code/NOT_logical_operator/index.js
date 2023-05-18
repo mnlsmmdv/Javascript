32ca608ac3d3d650d548a1dd3960459c0dfaca43
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-18 22:17:58 +0500

    Statement validates temperature

--- Bro_Code/NOT_logical_operator/index.js
+++ Bro_Code/NOT_logical_operator/index.js
@@ -5,3 +5,13 @@
 // Variable declaration for temperature and sunny weather.
 let temperature = 15;
 let sunny = true;
+
+// Validates temperature is not above 0°C.
+if(!(temperature > 0)){
+    // Message given to the user.
+    console.log("It is cold outside!");
+}
+else{
+    // Message given to the user.
+    console.log("It is warm outside!");
+}
