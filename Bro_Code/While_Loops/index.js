ea3e27ec39023cbe79c0e88bccd2afd932351a9d
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-22 02:12:57 +0500

    Variable declared to store username

--- Bro_Code/While_Loops/index.js
+++ Bro_Code/While_Loops/index.js
@@ -1,3 +1,6 @@
 // while loop = Repeat some code
 //              while some condition is true
 //              potentially infinite
+
+// Variable declaration to store username.
+let userName = "";
