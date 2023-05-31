a1e8b3ff718b876bf9aa15a0ad1e4e3b4926729f
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-31 23:24:36 +0500

    Counts up to 13 and breaks

--- Bro_Code/Break_Continue_Statements/index.js
+++ Bro_Code/Break_Continue_Statements/index.js
@@ -1,2 +1,13 @@
 // break = Breaks out of a loop entirely
 // continue = Skip an iteration of a loop
+
+// Counts from 01-20 (break).
+for(i = 1; i <= 20; i += 1){
+    // Breaks count at 13.
+    if(i == 13){
+        break;
+    }
+
+    // Displays the count up to 12.
+    console.log(i);
+}
