2abc42ea260e5d15ebb13e3723ecd62c17308d1b
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-31 16:10:40 +0500

    Displays count from 10-01

--- Bro_Code/For_Loops/index.js
+++ Bro_Code/For_Loops/index.js
@@ -6,3 +6,9 @@ for(let i = 1; i <= 10; i += 1){
     // Displays variable.
     console.log(i);
 }
+
+// Displays count from 10-01.
+for(let counter = 10; counter = 0; counter -= 1){
+    // Displays variable.
+    console.log(counter);
+}
