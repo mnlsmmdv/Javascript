df63c0ce37f0296842b8fc2ec07df6b788a5b69e
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-06-01 02:36:50 +0500

    Counts from 01-03 using nested for loops

--- Bro_Code/Nested_Loops/index.js
+++ Bro_Code/Nested_Loops/index.js
@@ -5,3 +5,12 @@ for(let i = 1; i <= 3; i += 1){
     // Displays the count 01-03.
     console.log(i);
 }
+
+// Counts from 01-03 (nested).
+// First For loop iterates three times.
+for(let i = 1; i <= 3; i += 1){
+    for(let j = 1; j <= 3; j += 1){
+        // Displays the count from 01-03.
+        console.log(j);
+    }
+}
