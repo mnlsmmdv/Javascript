5c0ebab2b8dc83adc8df8e9ab059335a3841ff0e
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-31 23:27:05 +0500

    Code commented out

--- Bro_Code/Break_Continue_Statements/index.js
+++ Bro_Code/Break_Continue_Statements/index.js
@@ -11,3 +11,16 @@ for(i = 1; i <= 20; i += 1){
     // Displays the count up to 12.
     console.log(i);
 }
+
+/*
+// Counts from 01-20 (continue).
+for(i = 1; i <= 20; i += 1){
+    // Continues from 14 after taking out 13.
+    if(i == 13){
+        continue;
+    }
+
+    // Displays the count 01-20 without 13.
+    console.log(i);
+}
+*/
