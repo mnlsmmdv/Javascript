6d753dfce04f3278ee903ba41434807633288c6c
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-05-31 16:11:16 +0500

    Code commented out

--- Bro_Code/For_Loops/index.js
+++ Bro_Code/For_Loops/index.js
@@ -7,8 +7,10 @@ for(let i = 1; i <= 10; i += 1){
     console.log(i);
 }
 
+/*
 // Displays count from 10-01.
 for(let counter = 10; counter = 0; counter -= 1){
     // Displays variable.
     console.log(counter);
 }
+*/
