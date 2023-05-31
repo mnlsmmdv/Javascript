3b73af4eb05bb417be8b337eb8b8a510e99792f2
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-06-01 02:38:01 +0500

    Code commented out

--- Bro_Code/Nested_Loops/index.js
+++ Bro_Code/Nested_Loops/index.js
@@ -1,10 +1,12 @@
 // nested loop = A loop inside of an other loop.
 
+/*
 // Counts from 01-03.
 for(let i = 1; i <= 3; i += 1){
     // Displays the count 01-03.
     console.log(i);
 }
+*/
 
 // Counts from 01-03 (nested).
 // First For loop iterates three times.
