76d190c310a0e6186c8bd55d3ff16207889c9504
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-06-02 23:02:58 +0500

    Displays birthday message once

--- Bro_Code/Functions/index.js
+++ Bro_Code/Functions/index.js
@@ -1,6 +1,9 @@
 // function = Define code once, and use it many times.
 //            To perform some code, call the function name.
 
+// Function called to display birthday message once.
+happyBirthday();
+
 // This function displays the Happy Birthday song in three iterations.
 function happyBirthday(){
     // Displays birthday message.
