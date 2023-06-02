8c5155354c13edd7d5672f06a46f758fa2ab0f37
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-06-03 02:09:16 +0500

    Function will now calculate the area

--- Bro_Code/Return_Statement/index.js
+++ Bro_Code/Return_Statement/index.js
@@ -9,3 +9,9 @@ let rectangleHeight;
 // User prompts to enter width and height.
 rectangleWidth = window.prompt("Enter Width: ");
 rectangleHeight = window.prompt("Enter Height: "); 
+
+// This function calculates the area inside of a rectangle.
+function calculateArea(rectangleWidth, rectangleHeight){
+    // Calculates the area.
+    return rectangleWidth * rectangleHeight;
+}
