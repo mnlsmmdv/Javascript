b8351535f12bd091695551a0f4cbc1f60aceeced
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-06-02 23:02:08 +0500

    Function declared

--- Bro_Code/Functions/index.js
+++ Bro_Code/Functions/index.js
@@ -1,2 +1,12 @@
 // function = Define code once, and use it many times.
 //            To perform some code, call the function name.
+
+// This function displays the Happy Birthday song in three iterations.
+function happyBirthday(){
+    // Displays birthday message.
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday dear ___");
+    console.log("Happy birthday to you!");
+    console.log("You are ___ years old!");
+}
