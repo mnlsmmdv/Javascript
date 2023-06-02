475d8e3353fb783dd9c454f0535a02378703976a
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-06-03 02:12:03 +0500

    Function called and displays area inside of rectangle

--- Bro_Code/Return_Statement/index.js
+++ Bro_Code/Return_Statement/index.js
@@ -8,7 +8,13 @@ let rectangleHeight;
 
 // User prompts to enter width and height.
 rectangleWidth = window.prompt("Enter Width: ");
-rectangleHeight = window.prompt("Enter Height: "); 
+rectangleHeight = window.prompt("Enter Height: ");
+
+// Function call assigned to variable.
+rectangleArea = calculateArea(rectangleWidth, rectangleHeight);
+
+// Displays the area inside of a rectangle.
+console.log("Area inside of Rectangle: ", rectangleArea);
 
 // This function calculates the area inside of a rectangle.
 function calculateArea(rectangleWidth, rectangleHeight){
