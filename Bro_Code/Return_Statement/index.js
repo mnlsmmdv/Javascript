61aec51f9cade10e911993a49a96c17975ffe489
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-06-03 01:59:00 +0500

    Variables declared for area, width and height

--- Bro_Code/Return_Statement/index.js
+++ Bro_Code/Return_Statement/index.js
@@ -1,2 +1,7 @@
 // return = Returns a value back to the place
 //          where you invoked a function
+
+// Variables declared to values of store area, width and height.
+let rectangleArea;
+let rectangleWidth;
+let rectangleHeight;
