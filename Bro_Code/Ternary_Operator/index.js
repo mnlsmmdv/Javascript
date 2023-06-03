de24e07b05ff029fa79eab60de3aaa11a3339733
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-06-03 14:20:54 +0500

    Comments added

---
+++ Bro_Code/Ternary_Operator/index.js
@@ -0,0 +1,8 @@
+// ternary operator = Shortcut for an 'if/ else statement'
+//                    Takes 3 operands
+                     
+//                    1. a condition with ?
+//                    2. expression if True .
+//                    3. expression if False
+
+// condition ? exprIfTrue : exprIfFaIse
