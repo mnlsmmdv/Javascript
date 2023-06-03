f99ba4d71f1c24ceb327635662b57a0e2aa206aa
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-06-03 14:26:42 +0500

    Function declared to check age

--- Bro_Code/Ternary_Operator/index.js
+++ Bro_Code/Ternary_Operator/index.js
@@ -9,3 +9,16 @@
 
 // Function assigned to variable.
 let adult = checkAge(21);
+
+// This function will check a users age.
+function checkAge(){
+    // Checks if age is 18 and above.
+    if(age >= 18){
+        // User is 18 and above.
+        return true;
+    }
+    else{
+        // User is below 18.
+        return false;
+    }
+}
