cadf3bfa9674cdfdd5e2bc756ebfc6ace9cea7d9
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-06-05 01:08:15 +0500

    Let keyword example

--- Bro_Code/Var_vs_Let/index.js
+++ Bro_Code/Var_vs_Let/index.js
@@ -2,3 +2,10 @@
  
 // let = variables are limited to block scope {}
 // var = variables are limited to a function(){}
+
+// This For loop counts up to 03.
+// let - Example.
+for(let i = 1; i <= 3; i += 1){
+    // Displaying the count.
+    console.log(i);
+}
