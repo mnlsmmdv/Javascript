0931e98d7b7696eaf35c6c3db3160e11b4e2cfe4
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-06-16 23:16:18 +0500

    Variables declared

--- Bro_Code/Template_Literals/index.js
+++ Bro_Code/Template_Literals/index.js
@@ -1,3 +1,8 @@
 // Template literals = delimited with (`)
 //                     instead of double or single quotes
 //                     allows embedded variables and expressions
+
+// Variable declarations for online store example.
+let userName = "Affaan";
+let items = 3;
+let total = 75;
