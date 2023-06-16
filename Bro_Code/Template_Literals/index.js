24ab66be874b86cba4836232992a203e14790afe
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-06-16 23:22:02 +0500

    Variable values displayed using template literals

--- Bro_Code/Template_Literals/index.js
+++ Bro_Code/Template_Literals/index.js
@@ -6,3 +6,8 @@
 let userName = "Affaan";
 let items = 3;
 let total = 75;
+
+// Displaying values of variables using Template Literals.s
+console.log(`Hello ${userName}.`);
+console.log(`You have ${items} items in your cart!`);
+console.log(`Your total is ${total}`);
