3fece155bd622f78438127618176c833464c18f3
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-07-07 09:15:16 +0500

    New file created

---
+++ Bro_Code/Format_Currency/index.js
@@ -0,0 +1,7 @@
+// toLocaleString() = Returns a string with a language
+//                    sensitive representation of this number
+
+// number.toLocaleString(locale, {option});
+
+// 'locale' = Specify that language (undefined = default set in browser)
+// 'options' = Object with formatting options
