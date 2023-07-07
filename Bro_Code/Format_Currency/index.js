5a9949cf1bcbe6a0770ffb02f0089be0326d9c6e
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-07-08 03:35:20 +0500

    Variable declared to display currency locale

--- Bro_Code/Format_Currency/index.js
+++ Bro_Code/Format_Currency/index.js
@@ -11,6 +11,7 @@ let myNumberOne = 123456.789;
 let myNumberTwo = 123456.789;
 let myNumberThree = 123456.789;
 let myCurrencyOne = 123456.789;
+let myCurrencyTwo = 123456.789;
 
 // US locale format (US English).
 myNumberOne = myNumberOne.toLocaleString("en-US");
